a6bd9a8fac1991691042b0b2c55785e3ba3d9ac9
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-01 00:21:56 +0530

    Pilot - Created basic project structure

---
+++ src/main/java/com/api/blog/services/impl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.api.blog.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.api.blog.entities.User;
+import com.api.blog.payloads.UserDto;
+import com.api.blog.repositories.UserRepo;
+import com.api.blog.services.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+
+		User user = this.dtoToUser(userDto);
+		User savedUser = this.userRepo.save(user);
+		return this.userToDto(savedUser);
+	}
+
+	@Override
+	public UserDto updateUser(UserDto user, Integer id) {
+		return null;
+	}
+
+	@Override
+	public UserDto getUserById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Integer id) {
+
+	}
+
+	private User dtoToUser(UserDto userDto) {
+		User user = new User();
+		user.setId(userDto.getId());
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setAbout(userDto.getAbout());
+		user.setPassword(userDto.getPassword());
+
+		return user;
+
+	}
+
+	public UserDto userToDto(User user) {
+		UserDto userDto = new UserDto();
+		userDto.setId(user.getId());
+		userDto.setName(user.getName());
+		userDto.setEmail(user.getEmail());
+		userDto.setAbout(user.getAbout());
+		userDto.setPassword(user.getPassword());
+
+		return userDto;
+	}
+
+}
