4cca2cc83f073961769d6fe214609402caef2344
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-02 01:36:25 +0530

    Implemented Create User Controller

---
+++ src/main/java/com/api/blog/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.api.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.blog.payloads.UserDto;
+import com.api.blog.services.impl.UserServiceImpl;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+
+	@PostMapping("/")
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+
+		UserDto createdUserDto = this.userServiceImpl.createUser(userDto);
+		return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
+	}
+
+}
