7d24ee988dbd2640ab3989c6e749073c3488b2ad
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-04 00:07:25 +0530

    Implemented Update User Controller

--- src/main/java/com/api/blog/controllers/UserController.java
+++ src/main/java/com/api/blog/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.api.blog.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,13 @@ public class UserController {
 
 		UserDto createdUserDto = this.userServiceImpl.createUser(userDto);
 		return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer userId) {
+		UserDto updatedUser = this.userServiceImpl.updateUser(userDto, userId);
+
+		return new ResponseEntity<UserDto>(updatedUser, HttpStatus.OK);
 	}
 
 }
