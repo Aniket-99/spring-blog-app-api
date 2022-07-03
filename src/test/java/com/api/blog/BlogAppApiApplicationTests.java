b9a9ac6281029028eba677277dc6430976e05060
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-04 00:06:45 +0530

    Testing the UserRepo interface proxy class wiring

--- src/test/java/com/api/blog/BlogAppApiApplicationTests.java
+++ src/test/java/com/api/blog/BlogAppApiApplicationTests.java
@@ -1,13 +1,30 @@
 package com.api.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.api.blog.repositories.UserRepo;
 
 @SpringBootTest
 class BlogAppApiApplicationTests {
 
+	
+	@Autowired
+	private UserRepo userRepo;
+	
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	
+	@Test
+	public void repoTest() {
+		String className = this.userRepo.getClass().getName();
+		String packageName = this.userRepo.getClass().getPackageName();
+		
+		System.out.println(className + " "+ packageName);
 	}
 
 }
