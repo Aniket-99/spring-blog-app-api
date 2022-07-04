a0170449532d4620634d454e7a442991eb7df2e6
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-05 00:45:32 +0530

    Added Delete,GetAll & GetSingleUser Controller methods

--- src/main/java/com/api/blog/controllers/UserController.java
+++ src/main/java/com/api/blog/controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.api.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.blog.payloads.ApiResponse;
 import com.api.blog.payloads.UserDto;
 import com.api.blog.services.impl.UserServiceImpl;
 
@@ -31,6 +36,28 @@ public class UserController {
 		UserDto updatedUser = this.userServiceImpl.updateUser(userDto, userId);
 
 		return new ResponseEntity<UserDto>(updatedUser, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer userId) {
+		this.userServiceImpl.deleteUser(userId);
+
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully", true), HttpStatus.OK);
+
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUsers() {
+
+		List<UserDto> allUsers = this.userServiceImpl.getAllUsers();
+
+		return new ResponseEntity<List<UserDto>>(allUsers, HttpStatus.OK);
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable("userId") Integer userId) {
+		UserDto user = this.userServiceImpl.getUserById(userId);
+		return new ResponseEntity<UserDto>(user, HttpStatus.OK);
 	}
 
 }
