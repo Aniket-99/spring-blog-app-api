41093292c382f88182930e6be062ccd32a7dc31d
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-10 01:10:04 +0530

    Added Category Entity

---
+++ src/main/java/com/api/blog/entities/Category.java
@@ -0,0 +1,63 @@
+package com.api.blog.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int categoryId;
+
+	@Column(nullable = false)
+	private String categoryTitle;
+
+	@Column(nullable = false)
+	private String categoryDescription;
+
+	public Category() {
+		super();
+	}
+
+	public Category(int categoryId, String categoryTitle, String categoryDescription) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryTitle = categoryTitle;
+		this.categoryDescription = categoryDescription;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryTitle() {
+		return categoryTitle;
+	}
+
+	public void setCategoryTitle(String categoryTitle) {
+		this.categoryTitle = categoryTitle;
+	}
+
+	public String getCategoryDescription() {
+		return categoryDescription;
+	}
+
+	public void setCategoryDescription(String categoryDescription) {
+		this.categoryDescription = categoryDescription;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [categoryId=" + categoryId + ", categoryTitle=" + categoryTitle + ", categoryDescription="
+				+ categoryDescription + "]";
+	}
+
+}
