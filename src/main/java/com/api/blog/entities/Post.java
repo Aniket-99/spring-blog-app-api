f7f0b34be481aad7cf9d9618b2454ad5fcb971b1
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-11 23:37:08 +0530

    Added Post Entity

---
+++ src/main/java/com/api/blog/entities/Post.java
@@ -0,0 +1,117 @@
+package com.api.blog.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int postId;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false, length = 10000)
+	private String content;
+
+	@Column(nullable = false)
+	private String imageName;
+
+	@Column(nullable = false)
+	private Date addedDate;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToOne
+	private User user;
+
+	public Post() {
+		super();
+	}
+
+	public Post(int postId, String title, String content, String imageName, Date addedDate, Category category,
+			User user) {
+		super();
+		this.postId = postId;
+		this.title = title;
+		this.content = content;
+		this.imageName = imageName;
+		this.addedDate = addedDate;
+		this.category = category;
+		this.user = user;
+	}
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [postId=" + postId + ", title=" + title + ", content=" + content + ", imageName=" + imageName
+				+ ", addedDate=" + addedDate + ", category=" + category + ", user=" + user + "]";
+	}
+
+}
