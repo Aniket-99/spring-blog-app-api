6c7719b6d3c4c92e8121e18c96785db4924d7254
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-16 01:37:37 +0530

    Added PostDto and PostRepo class

---
+++ src/main/java/com/api/blog/payloads/PostDto.java
@@ -0,0 +1,87 @@
+package com.api.blog.payloads;
+
+import java.util.Date;
+
+public class PostDto {
+
+	private String title;
+
+	private String content;
+
+	private String imageName;
+
+	private Date addedDate;
+
+	private UserDto user;
+
+	private CategoryDto category;
+
+	public PostDto() {
+		super();
+	}
+
+	public PostDto(String title, String content, String imageName, Date addedDate, UserDto user, CategoryDto category) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.imageName = imageName;
+		this.addedDate = addedDate;
+		this.user = user;
+		this.category = category;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public UserDto getUser() {
+		return user;
+	}
+
+	public void setUser(UserDto user) {
+		this.user = user;
+	}
+
+	public CategoryDto getCategory() {
+		return category;
+	}
+
+	public void setCategory(CategoryDto category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "PostDto [title=" + title + ", content=" + content + ", imageName=" + imageName + ", addedDate="
+				+ addedDate + ", user=" + user + ", category=" + category + "]";
+	}
+
+}
