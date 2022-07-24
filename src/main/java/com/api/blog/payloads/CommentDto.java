0f4a9c7bbe8f7d464317db15d43426ce219144b7
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-25 00:56:50 +0530

    Added CommentDto - Data transfer object

---
+++ src/main/java/com/api/blog/payloads/CommentDto.java
@@ -0,0 +1,40 @@
+package com.api.blog.payloads;
+
+public class CommentDto {
+
+	private int id;
+
+	private String content;
+
+	public CommentDto() {
+		super();
+	}
+
+	public CommentDto(int id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "CommentDto [id=" + id + ", content=" + content + "]";
+	}
+
+}
