f88eddfad692c85a06ba718070ff3e5225450c91
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-27 00:56:20 +0530

    Added findByEmail DSL method

--- src/main/java/com/api/blog/repositories/UserRepo.java
+++ src/main/java/com/api/blog/repositories/UserRepo.java
@@ -1,11 +1,15 @@
 package com.api.blog.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.api.blog.entities.User;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Integer>{
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+	Optional<User> findByEmail(String email);
 
 }
