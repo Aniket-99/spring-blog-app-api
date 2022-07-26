e17c1c6e0d6933343e2ebfd3be2ad3a1bc3ab45c
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-07-27 01:01:03 +0530

    Removed ModelMapper bean and added it in AppConfig class

--- src/main/java/com/api/blog/BlogAppApiApplication.java
+++ src/main/java/com/api/blog/BlogAppApiApplication.java
@@ -1,9 +1,7 @@
 package com.api.blog;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogAppApiApplication {
@@ -12,9 +10,5 @@ public class BlogAppApiApplication {
 		SpringApplication.run(BlogAppApiApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
 
 }
